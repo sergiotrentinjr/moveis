35f9d28b29316ad0ad53272e2aeba5c027d16072
Sergio Trentin Jr <sergiotrentinjr@gmail.com>
2019-04-09 02:16:08 -0300

    Alterado o index.html para corrigir acentuação

--- principal.js
+++ principal.js
@@ -1,4 +1,4 @@
-var banners = ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+﻿var banners = ["Os melhores do Brasil!", "Qualidade e pre&ccedil;o baixo!"];
 var bannerAtual = 0;
 
 function trocaBanner(){
